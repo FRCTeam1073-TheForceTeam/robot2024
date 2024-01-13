16656798d616287e3c648ddd26971fed6b03d9cb
oli-24 <gstrmllr24@gmail.com>
2024-01-13 16:04:57 -0500

    Cleaned up bling
    Removed unused flags and duplicate methods and corrected comments

--- src/main/java/frc/robot/subsystems/Bling.java
+++ src/main/java/frc/robot/subsystems/Bling.java
@@ -9,20 +9,15 @@ import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.RobotController;
 
 public class Bling extends Diagnostics {
-  /** Creates a new Bling. 
-   * Creates variables
-  */
   public AddressableLED m_led;
   public AddressableLEDBuffer m_ledBuffer;
 
+  public int length = 24;
   public int slotLength;
-
-  public boolean cleared;
-
-  public int ledR = 0;
-  public int ledG = 0;
-  public int ledB = 0;
+  public int numSlots = 8;
+  
   /**
+   * Creates a new bling.
    * @param key - the key name
    * @param defaultValue - the value to be returned if no value is found
    * @param length - the strip length
@@ -35,34 +30,39 @@ public class Bling extends Diagnostics {
    */
   public Bling() {
     m_led = new AddressableLED(0);
-    m_ledBuffer = new AddressableLEDBuffer(24);
+    m_ledBuffer = new AddressableLEDBuffer(length);
     m_led.setLength(m_ledBuffer.getLength());
     m_led.setData(m_ledBuffer);
     m_led.start();
-    slotLength = (int) (m_ledBuffer.getLength() / (8));
+    slotLength = (int) (m_ledBuffer.getLength() / (numSlots));
   }
 
   /**
-   * Clears all of the LEDs on the robot
+   * Clears all of the LEDs on the robot.
    */
   public void initialize() {
     clearLEDs();
   }
 
   /**
-   * Sets up battary bling
-   * and adds a buffer
+   * Sets the buffer and runs battery bling.
    */
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     m_led.setData(m_ledBuffer);
-    batteryBling(0);
-    
+    setBatteryBling(0);
   }
 
   /**
-   * Is capable of setting all of the the RGBs to one color (including battery bling)
+   * @return LED buffer
+   */
+  public AddressableLEDBuffer getM_LEDBuffer() {
+    return m_ledBuffer;
+  }
+
+  /**
+   * Sets one LED to a color
    * @param i - the index to write
    * @param r - the r value [0-255]
    * @param g - the g value [0-255]
@@ -73,108 +73,35 @@ public class Bling extends Diagnostics {
     m_ledBuffer.setRGB(i, r, g, b);
   }
 
-  // Initialize preferences for this class:
-  public static void initPreferences() {
-  
-  }
-  
   /**
-   * 
-   * @return the results
-   */
-  @Override
-  public void runDiagnostics() {
-    String result = new String();
-    boolean isOK = true;
-    //TODO: run diagnostics here
-    super.setDiagnosticResult(result);
-    super.setOK(isOK);
-  }
-
-  /**
-   * 
-   * @return LED buffer
-   */
-  public AddressableLEDBuffer getM_LEDBuffer() {
-    return m_ledBuffer;
-  }
-
-  /**
-   * Boolean to see if the LED's have been cleared
-   */
-  public void cleared() {
-    cleared = true;
-  }
-
-  /**
-   * Boolean to see if the LED's have been cleared
-   */
-  public void uncleared() {
-    cleared = false;
-  }
-
-  /**
-   * Clears all of the LEDs to be colorless
-   */
-  public void clearLEDs() {
-    setColorRGBAll(0, 0, 0);
-  }
-
-  /**
-   * Can set all of the LEDs to one color
-   * @param i - the index to write
-   * @param r - the r value [0-255]
-   * @param g - the g value [0-255]
-   * @param b - the b value [0-255]
-   */
-  public void setLED(int i, int r, int g, int b) {
-    m_ledBuffer.setRGB(i, r, g, b);
-    m_led.setData(m_ledBuffer);
-  }
-
-
-
-  // This sets two leds with the same color
-  /**
-   * 
-   * @param i - the index to write
-   * @param i2 - the index to write
-   * @param r - the r value [0-255]
-   * @param g - the g value [0-255]
-   * @param b - the b value [0-255]
-   */
-  public void setLEDs2(int i, int i2, int r, int g, int b) {
-    m_ledBuffer.setRGB(i, r, g, b);
-    m_ledBuffer.setRGB(i2, r, g, b);
-    // m_led.setData(m_ledBuffer);
-  }
-
-
-
-  // setColorRGBAll sets the LEDs all to one color
-  /**
+   * Sets all the LEDs to one color
    * @param r - the r value [0-255]
    * @param g - the g value [0-255]
    * @param b - the b value [0-255]
    * @return buffer length
    */
-  public void setColorRGBAll(int r, int g, int b) {
+  public void setRGBAll(int r, int g, int b) {
     for (var i = 0; i < (m_ledBuffer.getLength()); i++) {
       m_ledBuffer.setRGB(i, r, g, b);
     }
-    m_led.setData(m_ledBuffer);
   }
 
-   // rangeRGB() sets a range of LEDs to one color
+  /**
+   * Turns off all LEDs
+   */
+  public void clearLEDs() {
+    setRGBAll(0, 0, 0);
+  }
+
    /**
-    * 
+    * Sets a range of LEDs to one color.
     * @param min
     * @param number
-   * @param r - the r value [0-255]
-   * @param g - the g value [0-255]
-   * @param b - the b value [0-255]
+    * @param r - the r value [0-255]
+    * @param g - the g value [0-255]
+    * @param b - the b value [0-255]
     */
-   public void rangeRGB(int min, int number, int r, int g, int b) {
+   public void setRangeRGB(int min, int number, int r, int g, int b) {
     if (number != 1) {
       int max = min + number;
       for (int i = min; i < (max); i++) {
@@ -186,55 +113,52 @@ public class Bling extends Diagnostics {
   }
 
   /**
-   * 
+   * Sets a slot of LEDs to one color.
    * @param slotNumber
    * @param r - the r value [0-255]
    * @param g - the g value [0-255]
    * @param b - the b value [0-255]
    */
-  public void setSlot(int slotNumber, int r, int g, int b) {
-    rangeRGB(slotNumber*slotLength, slotLength, r, g, b);
+  public void setSlotRGB(int slotNumber, int r, int g, int b) {
+    setRangeRGB(slotNumber*slotLength, slotLength, r, g, b);
 
   }
 
   /**
-   * Sets the battery bling to 
-   * green when battery is grater than 12
-   * Blue when battery is grater than 10
-   * Red is set to anything lower than 10
+   * Sets the battery bling to:
+   * Green when battery voltage is greater than 12
+   * Blue when battery voltage is greater than 10
+   * Red when battery voltage is less than or equal to 10
    * @param slotNumber
    */
-  public void batteryBling(int slotNumber) {
-    
-    
+  public void setBatteryBling(int slotNumber) {
     double volts = RobotController.getBatteryVoltage();
 
     if (volts > 12) {
-      setSlot(slotNumber, 0, 255, 0);
+      setSlotRGB(slotNumber, 0, 255, 0);
     }
     else if (volts > 10){
-      setSlot(slotNumber, 0, 0, 255);
+      setSlotRGB(slotNumber, 0, 0, 255);
     }
     else{
-      setSlot(slotNumber, 255, 0, 0);
+      setSlotRGB(slotNumber, 255, 0, 0);
     }
   }
 
-
+  // Initialize preferences for this class:
+  public static void initPreferences() {
+    //TODO: remove if not using preferences
+  }
+  
   /**
-   * @param r - the r value [0-255]
-   * @param g - the g value [0-255]
-   * @param b - the b value [0-255]
+   * @return diagnostic results
    */
-  public void setRGBAll(int r, int g, int b)
-  {
-    ledR = r;
-    ledG = g;
-    ledB = b;
-
-    for(int i = 0; i < m_ledBuffer.getLength(); i++)
-    {
-      m_ledBuffer.setRGB(i, r, g, b);
-    }
+  @Override
+  public void runDiagnostics() {
+    String result = new String();
+    boolean isOK = true;
+    //TODO: run diagnostics here
+    super.setDiagnosticResult(result);
+    super.setOK(isOK);
   }
 }
