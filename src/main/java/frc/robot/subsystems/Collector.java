1bdb4c96031375094f53cb7841b349d3312c4e52
Silverfox088 <mindi.h@frc1073.org>
2024-01-24 20:31:04 -0500

    Added a runDiagnostics method and implemented tof

--- src/main/java/frc/robot/subsystems/Collector.java
+++ src/main/java/frc/robot/subsystems/Collector.java
@@ -9,11 +9,24 @@ import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycle;
 
 public class Collector extends Diagnostics {
   TalonFX liftMotor = new TalonFX(0); // TODO: set device id
   TalonFX collectMotor = new TalonFX(0); // same thing
-  private double speed;
+  private double collectorSpeed;
+  private double liftSpeed;
+  private DigitalInput tof1;
+  private DutyCycle tof1DutyCycleInput;
+  private double tof1DutyCycle;
+  private double tof1Freq;
+  private double tof1Range;
+  private final double tof1ScaleFactor = 100000; 
+
+  private final double collectorTicksPerMeter = 1;
+  // private final double intakeGearRatio = 5.0;
+  // private final double intakeTicksPerRadian = 2048.0 * intakeGearRatio / (2.0 * Math.PI);
   
   // fill in PID values
   private double lift_kP = 0;
@@ -28,7 +41,13 @@ public class Collector extends Diagnostics {
 
   /** Creates a new Collector. */
   public Collector() {
-    speed = 0;
+    collectorSpeed = 0;
+    liftSpeed = 0;
+
+    tof1 = new DigitalInput(0); 
+    tof1DutyCycleInput = new DutyCycle(tof1);
+    tof1Freq = 0;
+    tof1Range = 0;
   }
 
   public void setUpMotors() {
@@ -53,14 +72,14 @@ public class Collector extends Diagnostics {
     collectMotor.getConfigurator().apply(collectMotorClosedLoopConfig);
 
   }
-  public void setLiftMotorSpeed(double speed)
+  public void setLiftMotorSpeed(double liftSpeed)
   {
-    liftMotor.setControl(new VelocityVoltage(speed));
+    liftMotor.setControl(new VelocityVoltage(liftSpeed));
   }
 
-  public void setCollectMotorSpeed(double speed)
+  public void setCollectMotorSpeed(double collectorSpeed)
   {
-    collectMotor.setControl(new VelocityVoltage(speed));
+    collectMotor.setControl(new VelocityVoltage(collectorSpeed * collectorTicksPerMeter));
   }
 
   public void setMotorSpeeds(double speed)
@@ -69,28 +88,72 @@ public class Collector extends Diagnostics {
     setCollectMotorSpeed(speed);
   }
 
-  public void setSpeed(double speed)
+
+  public void setCollectorSpeed(double collectorSpeed)
   {
-    this.speed = speed;
+    this.collectorSpeed = collectorSpeed;
   }
 
-  
-  public double getSpeed()
+  public double getCollectorSpeed()
   {
-    return speed;
+    return collectorSpeed;
   }
 
+  public double getRange1() {
+    return tof1Range;
+  }
+
+  public void setRange1(double tof1Range) {
+    this.tof1Range = tof1Range;
+  }
 
   @Override
   public void initSendable(SendableBuilder builder)
   {
     builder.setSmartDashboardType("Collector");
-    builder.addDoubleProperty("Speed", this::getSpeed, this::setSpeed);
+    builder.addDoubleProperty("Speed", this::getCollectorSpeed, this::setCollectorSpeed);
+    builder.setSmartDashboardType("tof1Range");
+    builder.addDoubleProperty("tof1Range", this::getRange1, this::setRange1);
   }
 
   @Override
   public void periodic() 
   {
-    setMotorSpeeds(speed);
+    setMotorSpeeds(collectorSpeed);
+    tof1Freq = tof1DutyCycleInput.getFrequency();
+    tof1DutyCycle = tof1DutyCycleInput.getOutput();
+    tof1DutyCycle = tof1DutyCycleInput.getOutput();
+    tof1Range = tof1ScaleFactor * (tof1DutyCycle / tof1Freq - 0.001);
+    
   }
+
+
+  @Override
+  public void runDiagnostics() 
+  {
+    setOK(true);
+  }
+
+  // public String getDiagnostics(){
+  //   String result = "";
+  //   Faults faults = new Faults();
+  //   collectorMotor.getFaults(faults);
+
+  //   if(faults.hasAnyFault()){
+  //     result += faults.toString();
+  //   }
+  //   ErrorCode error = collectorMotor.clearStickyFaults(500);
+  //   if (error != ErrorCode.OK) {
+  //     result += String.format("can't clear collectorMotor faults");
+  //   }
+
+  //   if(tof1DutyCycleInput.getFrequency()< 2){
+  //     result += String.format("tof1 not working");
+  //   }
+  //   // if(tof2DutyCycleInput.getFrequency()< 2){
+  //   //   result += String.format("tof2 not working");
+  //   // }
+        
+  //   return result;
+
 }
