6dca9927f2698f0fc558eeb612a386b6698451a6
oli-24 <84802703+spaceghost24@users.noreply.github.com>
2024-02-03 16:20:03 -0500

    probably easier without tables

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,6 +4,11 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.IntegerArrayPublisher;
+import edu.wpi.first.networktables.IntegerArraySubscriber;
+import edu.wpi.first.networktables.IntegerArrayTopic;
+import edu.wpi.first.networktables.PubSubOption;
 
 public class Drivetrain extends Diagnostics{
   /** Creates a new Drivetrain. */
@@ -14,3 +19,14 @@ public class Drivetrain extends Diagnostics{
     // This method will be called once per scheduler run
   }
 }
+
+// class AprilTagSubscriber {
+//   // the publisher is an instance variable so its lifetime matches that of the class
+//   IntegerArraySubscriber intArraySub;
+  
+//   public void GetAprilTag(IntegerArrayTopic intArrayTopic) {
+//       // start publishing; the return value must be retained (in this case, via
+//       // an instance variable)
+//       intArraySub = intArrayTopic.subscribe();
+//     }
+//   }
--- src/main/java/frc/robot/subsystems/OpenMV.java
+++ src/main/java/frc/robot/subsystems/OpenMV.java
@@ -11,9 +11,10 @@ import java.util.List;
 
 //import edu.wpi.first.networktables.DoubleEntry;
 import edu.wpi.first.networktables.IntegerArrayTopic;
+import edu.wpi.first.networktables.IntegerArrayPublisher;
 import edu.wpi.first.networktables.NetworkTablesJNI;
 import edu.wpi.first.networktables.PubSubOption;
-import edu.wpi.first.networktables.TimestampedDouble;
+// import edu.wpi.first.networktables.TimestampedDouble;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -33,12 +34,12 @@ public class OpenMV extends SubsystemBase{
       port = null;
     }
   }
+  public ArrayList<Byte> msg = new ArrayList();
 
 
-  public void getAprilTag() {
+  public void sendAprilTag() {
     byte[] cmdBytes = "1,a\n".getBytes();
     int bytesWritten = port.write(cmdBytes, cmdBytes.length);
-
   }
 
   public ArrayList<Byte> getMsg()
@@ -46,7 +47,7 @@ public class OpenMV extends SubsystemBase{
     ArrayList<Byte> msg = new ArrayList();
     byte[] oneByte = port.read(1);
     if (oneByte == "\n".getBytes()) {
-      return msg.toArray(new byte[0]);
+      return msg;
     }
     else {
       byte oneActualByte = oneByte[0];
@@ -58,82 +59,43 @@ public class OpenMV extends SubsystemBase{
   @Override
   public void periodic() {
     int bytesWaiting = port.getBytesReceived();  // returns the number of bytes waiting to be read, without actually reading them
-    byte[] cmdBytes = "2,g,0,1,2,3,4,5,6,7,8,9\n".getBytes("ASCII");
-    int cmdBytesLen = cmdBytes.length;
-    Integer wrote = port.write(cmdBytes, cmdBytesLen); // second arg is maximum bytes to write, which isn't a big deal for us
-    System.out.println(String.format("just wrote this many bytes: %d", wrote));
+    // byte[] cmdBytes = "2,g,0,1,2,3,4,5,6,7,8,9\n".getBytes("ASCII");
+    //int cmdBytesLen = cmdBytes.length;
+    //Integer wrote = port.write(cmdBytes, cmdBytesLen); // second arg is maximum bytes to write, which isn't a big deal for us
+    //System.out.println(String.format("just wrote this many bytes: %d", wrote));
+
     try {
       Thread.sleep(1000);
     }
     catch (final InterruptedException e) {
       throw new RuntimeException(e);
     }
-    ArrayList<Byte> msg = getMsg();
+    //ArrayList<Byte> msg = getMsg();
+    this.msg = getMsg();
     //String msgString = msg.toString();
-    char cmdChar = (char)msg[2];
-    if (cmdChar == 'a') {
-      //do whatever apriltags do
+    // char cmdChar = (char)msg[2];
+    // if (cmdChar == 'a') {
+    //   //do whatever apriltags do
   }
 }
-
-
-
-
-
-
-
-      // read(1);
-      // System.out.println(String.format("recvd: %s", recvd));
-      // byte[] thebytes = port.read(1);
-      // System.out.println(String.format("byte: %d", thebytes[0]));
-      // int val = thebytes[0] & 0x00ff;
-      // System.out.println(val);
-
-
-
-  public class AprilTagPublisher {
-  // the publisher is an instance variable so its lifetime matches that of the class
-  final intArrayPublisher intArrayPub;
-
-  public PublishAprilTag(intArrayTopic intArrayTopic) {
-    // start publishing; the return value must be retained (in this case, via
-    // an instance variable)
-    intArrayPub = intArrayTopic.publish(TagInfo);
-
-    // publish options may be specified using PubSubOption
-    intArrayPub = intArrayTopic.publish(PubSubOption.keepDuplicates(true));
-
-    // publishEx provides additional options such as setting initial
-    // properties and using a custom type string. Using a custom type string for
-    // types other than raw and string is not recommended. The properties string
-    // must be a JSON map.
-   // intArrayPub = intArrayTopic.publishEx("double", "{\"myprop\": 5}");
-  }
-
-  // public void periodic() {
-  //   // publish a default value
-  //   intArrayPub.setDefault(0.0);
-
-  //   // publish a value with current timestamp
-  //   intArrayPub.set(1.0);
-  //   intArrayPub.set(2.0, 0);  // 0 = use current time
-
-  //   // publish a value with a specific timestamp; NetworkTablesJNI.now() can
-  //   // be used to get the current time. On the roboRIO, this is the same as
-  //   // the FPGA timestamp (e.g. RobotController.getFPGATime())
-  //   long time = NetworkTablesJNI.now();
-  //   intArrayPub.set(3.0, time);
-
-  //   // publishers also implement the appropriate Consumer functional interface;
-  //   // this example assumes void myFunc(DoubleConsumer func) exists
-  //   myFunc(intArrayPub);
-  // }
-
-  // // often not required in robot code, unless this class doesn't exist for
-  // // the lifetime of the entire robot program, in which case close() needs to be
-  // // called to stop publishing
-  public void close() {
-    // stop publishing
-    intArrayPub.close();
-  }
 }
+// class AprilTagPublisher {
+//   // the publisher is an instance variable so its lifetime matches that of the class
+//   IntegerArrayPublisher intArrayPub;
+
+//   public void PublishAprilTag(IntegerArrayTopic intArrayTopic) {
+//     // start publishing; the return value must be retained (in this case, via
+//     // an instance variable)
+//     //int[] someintarray = [];
+//     intArrayPub = intArrayTopic.publish();
+
+//     // publish options may be specified using PubSubOption
+//     // intArrayPub = intArrayTopic.publish(PubSubOption.keepDuplicates(true));
+//   }
+
+
+//   public void close() {
+//     // stop publishing
+//     intArrayPub.close();
+//   }
+// }
