7144494348debb31fb1ff5f74672120e28f7df70
SnowballGus <william.vanuitert@gmail.com>
2024-02-19 18:20:08 -0500

    adding new files from the vision branch to the MotionSchema branch so we can get vision code and code that drives the robot on the same branch. still need changes to RobotContainer.java etc.

---
+++ src/main/java/frc/robot/commands/StartRecordingAutonomous.java
@@ -0,0 +1,57 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Camera;
+
+public class StartRecordingAutonomous extends Command {
+  Camera camera;
+  Boolean weAreFinished;
+  int initcounter;
+  int execcounter;
+  /** Creates a new StartCamera. */
+  public StartRecordingAutonomous(Camera camera) {
+    this.camera = camera;
+    addRequirements(camera);
+    this.weAreFinished = false;
+    this.initcounter = 0;
+    this.execcounter = 0;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    this.weAreFinished = false;
+    this.initcounter += 1;
+    System.out.println(String.format("StartRecordingAutonomous.java init counter: %s", this.initcounter));
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    this.weAreFinished = false;
+    System.out.println("in StartRecordingAutonomous.java execute(), starting autonomous recording");
+    camera.startRecordingAutonomous();
+    // TODO: listen for a reponse before finishing, ideally retry
+    this.weAreFinished = true;
+    this.execcounter += 1;
+    System.out.println(String.format("StartRecordingAutonomous.java exec counter: %s", this.execcounter));
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    System.out.println("in StartRecordingAutonomous.java end(), setting weAreFinished to true");
+    this.weAreFinished = true;
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return this.weAreFinished;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -0,0 +1,57 @@
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+// import java.util.ArrayList;
+import java.lang.Thread;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Camera extends SubsystemBase {
+  /** Creates a new Camera. */
+  private SerialComms serialComms;
+  private int camID;
+  
+  public Camera(SerialComms serialComms, int camID) {
+    System.out.println("in camera constructor");
+    this.serialComms = serialComms;
+    System.out.println("camID in constructor");
+    System.out.println(camID);
+  }
+
+  // public void getAprilTag(){
+  //   System.out.println("in getAprilTag()");
+  //   System.out.println("camID in getAprilTag()");
+  //   System.out.println(camID);
+  //   String cmdStr = "ti\n";
+  //   String data = SerialComms.transact(cmdStr);
+  //   System.out.println("gotAprilTag, data:");
+  //   System.out.println(data);
+  // }
+
+  public void startRecordingTeleop(){
+    serialComms.send("ti");
+  }
+
+  public void startRecordingAutonomous(){
+    serialComms.send("ai");
+  }
+
+  public void stopRecording(){
+    serialComms.send("di");
+  }
+
+  @Override
+  public void periodic() {
+    // System.out.println("getting apriltag data");
+    // getAprilTag();
+    // try {
+    //   Thread.sleep(1000);
+    // }
+    // catch (final InterruptedException e) {
+    //   throw new RuntimeException(e);
+    // }
+  }
+}
+
