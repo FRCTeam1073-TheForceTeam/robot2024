4e696c00a5b0f206c7d9f2ea77c673d5d833ee51
oli-24 <gstrmllr24@gmail.com>
2024-02-24 10:36:13 -0500

    Possible fixes?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,67 +4,30 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-// import frc.robot.subsystems.SerialComms;
-// import frc.robot.subsystems.SwerveModuleConfig;
-import edu.wpi.first.wpilibj.Preferences;
-
-// import com.ctre.phoenix6.mechanisms.swerve.SwerveModule;
-
-// import edu.wpi.first.wpilibj.SerialPort.Port;
-// import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-
-import frc.robot.commands.DriveThroughTrajectorySchema;
-import frc.robot.commands.DriveToPointSchema;
-import frc.robot.commands.SchemaDriveAuto;
-import frc.robot.subsystems.Bling;
-// import frc.robot.subsystems.Camera;
-import frc.robot.commands.TeleopDrive;
-import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.OI;
-// import frc.robot.subsystems.SerialComms;
-import frc.robot.subsystems.SwerveModuleConfig;
-
-import java.util.ArrayList;
-
-import edu.wpi.first.wpilibj.DriverStation;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.StartRecordingAutonomous;
-import frc.robot.commands.StartRecordingTeleop;
-import frc.robot.commands.StopRecording;
-//import frc.robot.subsystems.Camera;
-// import frc.robot.subsystems.SerialComms;
-import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-// import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-// import frc.robot.subsystems.Bling;
-// import frc.robot.subsystems.Camera;
-// import frc.robot.commands.TeleopDrive;
-// import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.*;
-import frc.robot.commands.PivotTestCommand;
-import frc.robot.commands.ShooterTestCommand;
-import frc.robot.commands.FeederTestCommand;
-import frc.robot.commands.LoadFeeder;
-import frc.robot.commands.RunFeeder;
-import frc.robot.commands.SetShooterAngle;
-import frc.robot.commands.RunShooter;
-// import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+
+import java.util.ArrayList;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -74,6 +37,7 @@ import frc.robot.commands.RunShooter;
  */
 public class RobotContainer {
   SerialPort.Port serial_port = SerialPort.Port.kUSB;
+
   // The robot's subsystems and commands are defined here...
   private final Pivot m_pivot = new Pivot();
   private final Shooter m_shooter = new Shooter();
@@ -89,9 +53,7 @@ public class RobotContainer {
   // private final SetShooterAngle setShooterAngle = new SetShooterAngle(m_feeder, 0);
   // private final RunShooter runShooter = new RunShooter(m_shooter, 0, 0, 0);
   private final TeleopDrive m_teleopCommand = new TeleopDrive(m_drivetrain, m_OI);
-  // SequentialCommandGroup fullAuto;
-  // private final SerialComms m_serial = new SerialComms(Port.kUSB1);
-  // private final Camera m_cammera = new Camera(m_serial, 1);
+  
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
   private static final String kNoAuto = "No Autonomous";
   // ex: private static final String auto1 = "auto 1";
@@ -99,16 +61,16 @@ public class RobotContainer {
   private static final String kLeaveAuto = "Leave Auto";
   private static final String kTestAuto = "Test Auto";
   
-  // private final SerialComms m_serial = new SerialComms(SerialPort.Port.kUSB);
-  // private final Camera m_camera1 = new Camera(m_serial, "1");  // camID is how SerialComms and the cameras themselves tells them apart
-  // private final Camera m_camera2 = new Camera(m_serial, "2");
+  private final SerialComms m_serial = new SerialComms(SerialPort.Port.kUSB);
+  private final Camera m_camera1 = new Camera(m_serial, "1");  // camID is how SerialComms and the cameras themselves tells them apart
+  private final Camera m_camera2 = new Camera(m_serial, "2");
   // private final StartRecordingAutonomous c_startRecordingAutonomous = new StartRecordingAutonomous(m_camera1);
   // private final StartRecordingTeleop c_startRecordingTeleop = new StartRecordingTeleop(m_camera1);
   // private final StopRecording c_stopRecording = new StopRecording(m_camera1);
   // redeclare for camera 2 until we figure out how to do more than one at a time
-  // private final StartRecordingAutonomous c_startRecordingAutonomous = new StartRecordingAutonomous(m_camera2);
-  // private final StartRecordingTeleop c_startRecordingTeleop = new StartRecordingTeleop(m_camera2);
-  // private final StopRecording c_stopRecording = new StopRecording(m_camera2);
+  private final StartRecordingAutonomous c_startRecordingAutonomous = new StartRecordingAutonomous(m_camera2);
+  private final StartRecordingTeleop c_startRecordingTeleop = new StartRecordingTeleop(m_camera2);
+  private final StopRecording c_stopRecording = new StopRecording(m_camera2);
   // and so on for however many cameras we have
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
@@ -185,8 +147,8 @@ public class RobotContainer {
   public static void initPreferences()
   {
     System.out.println("RobotContainer: init Preferences.");
-    // SwerveModuleConfig.initPreferences();
-    // Drivetrain.initPreferences();
+    SwerveModuleConfig.initPreferences();
+    Drivetrain.initPreferences();
     //OI.initPreferences();
     //SwerveModule.initPreferences();
   }
@@ -200,11 +162,33 @@ public class RobotContainer {
   }
 
   public Command getTeleopCommand(){
-    return null;
-    //return c_startRecordingTeleop;
+    return c_startRecordingTeleop;
   }
 
-   
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   * 
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    //TODO: addc_startRecordingAutonomous; to preexisting getAutonomousCommand
+    
+    switch (m_chooser.getSelected()){
+      case kNoAuto:
+        return null;
+      case kSnowPlowAuto:
+        return new SequentialCommandGroup(SchemaDriveAuto.create(new DriveToPointSchema(m_drivetrain, new Pose2d(7, 0, new Rotation2d(0)), 5, 1), m_drivetrain),
+          SchemaDriveAuto.create(new DriveToPointSchema(m_drivetrain, new Pose2d(7.0, 5.5, new Rotation2d(0)), 5, 1), m_drivetrain), c_startRecordingAutonomous);
+      case kLeaveAuto:
+        return new SequentialCommandGroup(SchemaDriveAuto.create(new DriveToPointSchema(m_drivetrain, new Pose2d(1.5, 0.0, new Rotation2d()), 1.5, 0), m_drivetrain),
+          c_startRecordingAutonomous);
+      case kTestAuto:
+        return new SequentialCommandGroup(testAuto(), c_startRecordingAutonomous);
+      default:
+        return null;
+    }
+    // An example command will be run in autonomous
+
     //return SchemaDriveAuto.create(new DriveToPointSchema(m_drivetrain, new Pose2d(-1.0, -1.0, new Rotation2d(0)), 0.5, 0.5), m_drivetrain);
     //return new DriveToPointSchema(m_drivetrain, new Pose2d(1.0, 0, new Rotation2d(Math.PI / 2)), 0.5, 0.5);
 
@@ -219,14 +203,10 @@ public class RobotContainer {
     //return SchemaDriveAuto.create(new DriveThroughTrajectorySchema(m_drivetrain, drivePoints, 0.5, 0.5, 0.5, 1.0), m_drivetrain);
   }
 
-  // public Command getDisabledCommand() {
-  //   return c_stopRecording;
-
-  // }
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return null;
+  public Command getDisabledCommand() {
+    return c_stopRecording;
   }
+
   public Command loadNoteToFeeder(){
     System.out.println("load motor is doing");
     return new LoadFeeder(m_feeder);
@@ -241,13 +221,11 @@ public class RobotContainer {
       new RunShooter(m_shooter, 20),
       new RunFeeder(m_feeder, 0)
     );
-  }
     // public Command launchNoteToSpeaker(){
     // return new SequentialCommandGroup(      
     //   new LoadFeeder(m_feeder, Preferences.getDouble("Feeder Target Max Speed", 0.3)),
     //   //new SetShooterAngle(m_shooter, Preferences.getDouble("Shooter Target Angle", 0.3)),
     //   new RunShooter(m_shooter, Preferences.getDouble("Shooter Target Max Speed", 0.3), 0, 0),   
     //   new RunFeeder(m_feeder, Preferences.getDouble("Feeder Target Max Speed", 0.3)));
-  //}
-
+  }
 }
