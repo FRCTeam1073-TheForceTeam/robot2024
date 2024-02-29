79e4138e85bc3f064f84286014b3a685c0f6a559
SnowballGus <william.vanuitert@gmail.com>
2024-02-28 19:35:10 -0500

    translations
    Co-Authored-By: C0leSc0ttB0t <107157923+C0leSc0ttB0t@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/AprilTagFinder.java
+++ src/main/java/frc/robot/subsystems/AprilTagFinder.java
@@ -12,6 +12,7 @@ import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.SerialComms;
 
@@ -20,98 +21,63 @@ import java.util.ArrayList;
 public class AprilTagFinder extends SubsystemBase {
   /** Creates a new GamePieceFinder. */
   public class Target {
+    // ID, X_translation, Y_Transtlation, and Z_translation
+    // Z_translation = far away
+    // Y_translation = height
+    // TODO x_translation = rotation???
     public String type;
-    public int ID = 0;
-    public double rotation = 0.0;
-    public double centerX = 0.0;
-    public double centerY = 0.0;
-    public double centerH = 0.0;
-    public double centerW = 0.0;
+    public static int ID = 0;
+    public static double X_translation = 0.0;
+    public static double Y_translation = 0.0;
+    public static double Z_translation = 0.0;
   }
 
-  private NetworkTable networkTable;
-  private NetworkTableEntry cubePieceEntry;
-  private NetworkTableEntry conePieceEntry;
-  public String tableName;
-  public double closestTagArea;
   public int closestTag;
   private Drivetrain driveSubsystem;
   private Transform3d cameraTransform;
   private GamePiecePoseEstimator poseEstimator;
-  public double closestTagX;
-  public double closestTagY;
   public ArrayList<Target> targets;
 
-  
-
   public class GamePiecePoseEstimator {
-    //public Transform3d estimate(){
-    //}
+    // public Transform3d estimate(){
+    // }
   }
 
   public AprilTagFinder(Drivetrain ds) {
     driveSubsystem = ds;
-    this.tableName = tableName;
+   
 
   }
 
   @Override
   public void periodic() {
     Number[] tagDataArray = cubePieceEntry.getNumberArray(new Number[0]);
-    int numTag = tagDataArray.length/7;
+    int numTag = tagDataArray.length / 4;
     // Reset search variables for clostest to empty:
 
-    for (int i = 0; i < numTag; i = i +1){
+    for (int i = 0; i < numTag; i = i + 1) {
       targets.clear();
       Target tagData = new Target();
 
-      tagData.centerX = tagDataArray[i*7 + 0].doubleValue();
-      tagData.centerY = tagDataArray[i*7 + 1].doubleValue();
-      tagData.centerW = tagDataArray[i*7 + 2].doubleValue();
-      tagData.centerH = tagDataArray[i*7 + 3].doubleValue();
-
+      tagData.ID = tagDataArray[i * 4 + 0].intValue();
+      tagData.X_translation = tagDataArray[i * 4 + 1].doubleValue();
+      tagData.Y_translation = tagDataArray[i * 4 + 2].doubleValue();
+      tagData.Z_translation = tagDataArray[i * 4 + 3].doubleValue();
       targets.add(tagData);
 
-      double area = tagData.centerW * tagData.centerH;
-      
-      if (area > closestTagArea) {
-        closestTagArea = area;
-        closestTag = i;
-      }
-
     }
-
-    if (closestTag > -1) {
-      closestTagX = (targets.get(closestTag).centerX) + (targets.get(closestTag).centerW/2);
-      closestTagY = (targets.get(closestTag).centerY) + (targets.get(closestTag).centerH/2);
-    }
-
-
 
     // if (!closestGamePiece.equals("None")){
-    //   SmartDashboard.putString(String.format("%s/ClosestGamePiece", tableName), closestGamePiece);
+    // SmartDashboard.putString(String.format("%s/ClosestGamePiece", tableName),
+    // closestGamePiece);
     // }else{
-    //   SmartDashboard.putString(String.format("%s/ClosestGamePiece", tableName), closestGamePiece);
+    // SmartDashboard.putString(String.format("%s/ClosestGamePiece", tableName),
+    // closestGamePiece);
     // }
-    SmartDashboard.putNumber("closestTag", closestTag);
-    SmartDashboard.putNumber("closestTagX", closestTagX);
-    SmartDashboard.putNumber("closestTagY", closestTagY);
-    SmartDashboard.putNumber("closestTagArea", closestTagArea);
-  }
-
-
-  public Pose2d getFieldPoseCube() {
-    if (targets.size() > 0) {
-      //TODO: Implement this method when needed
-      return null; // Until we implement it.
-    }else{
-      return null;
-    }
-  }
-
-
-  public int getClosestTag() {
-    return closestTag;
+    SmartDashboard.putNumber("ID", Target.ID);
+    SmartDashboard.putNumber("X_translation", Target.X_translation);
+    SmartDashboard.putNumber("Y_translation", Target.Y_translation);
+    SmartDashboard.putNumber("Z_translation", Target.Z_translation);
   }
 
 }
