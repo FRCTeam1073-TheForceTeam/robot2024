48b042e7433f75ba8b8f6401c691b30026a2decc
roboticsgeek <rtpack@charter.net>
2024-03-12 22:32:52 -0400

    Cleanup of new binary subsystem.

--- src/main/java/frc/robot/subsystems/AprilTagFinder.java
+++ src/main/java/frc/robot/subsystems/AprilTagFinder.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import java.util.HashMap;
 import java.util.Map;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -14,44 +15,33 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 //       note: periodic() wipes the map after ~0.5s without the camera returning any tags, whether it replies with no tags or stops replying entirely.
 public class AprilTagFinder extends SubsystemBase {
 
+  // April tag data
   public class TagData {
     public int id = -1;
     public int cx = 0;
     public int cy = 0;
     public int area = 0;
+    public double timestamp = 0;
   }
-
 
   public SerialComms serialcomms;
   public boolean waiting_for_response = false;
   public int wait_counter = 0;
   public int tagID = 4;  // use the SetAprilTagID to modify
-  public byte output_buffer[] = new byte[8];
-  public TagData tag_data = new TagData();
-  public int send_counter = 0;
-  public int recv_counter = 0;
-
-  // public Map<int , TagaData> tags = new HashMap<String, Map<String, String>>();
-
-  // https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html
-  // map example. given:
-  //   1. we're looking at april tags 2 and 4
-  //   2. we care about four stats for each tag (easy to adjust later)
-  //
-  // tags = {
-  //   "2": {"xcenter": "128", "ycenter": "178", "area": "57", "zaxisorwhatever": "45"},
-  //   "4": {"xcenter": "127", "ycenter": "177", "area": "56", "zaxisorwhatever": "44"}
-  // }
-  // example .get()'s:
-  // tags.get("2") == {"xcenter": "128", "ycenter": "178", "area": "57", "zaxisorwhatever": "45"}
-  // tags.get("9") == null
-  // caller can deal with null or call tags.containsKey("9") first.
+  public int camID = 1;  // Camera ID to send to
+  public byte outputBuffer[] = new byte[8];
+  public TagData tagData = new TagData();
+  public int sendCounter = 0;
+  public int recvCounter = 0;
 
   public AprilTagFinder(SerialComms serialcomms) {
     this.serialcomms = serialcomms;
     this.waiting_for_response = false;
     this.wait_counter = 0;
-    this.tag_data.id = -1;
+    this.tagData.id = -1;
+    for (int idx=0;idx < this.outputBuffer.length; ++idx) {
+      this.outputBuffer[idx] = 0;
+    }
   }
 
 
@@ -62,79 +52,76 @@ public class AprilTagFinder extends SubsystemBase {
 
   /// Returns the current tag data found.
   public TagData getTagData() {
-    return tag_data;
+    return tagData;
   }
 
 
-  // TODO: add System.currentTimeMillis() to inner maps, age out existing entries (do age outs before puts)
   public void parseResponse(byte[] response) {
-
     if (response.length < 8) {
       System.out.println(String.format("Invalid camera response length: %d", response.length));
-      this.tag_data.id = -1;
+      this.tagData.id = -1;
       return;
     }
 
-    tag_data.id = response[2]; // ID of the found tag.
-    tag_data.cx = response[3] * 4; // Undo packing so it fits a byte.
-    tag_data.cy = response[4] * 4; // Undo packing so it fits a byte.
-    tag_data.area = response[5] * 64; // Undo packing so it fits a byte.
+    tagData.id = response[2]; // ID of the found tag.
+    tagData.cx = response[3] * 4; // Undo packing so it fits a byte.
+    tagData.cy = response[4] * 4; // Undo packing so it fits a byte.
+    tagData.area = response[5] * 64; // Undo packing so it fits a byte.
+    tagData.timestamp = Timer.getFPGATimestamp();
   }
 
   @Override
   public void periodic() {
+
     SmartDashboard.putNumber("AprilTagFinderDesiredID", this.tagID);
-    // if -1, don't bother
+    // if -1, don't bother doing anything.
     if (this.tagID == -1) {
-      return;
-    }
-
-    if (this.waiting_for_response == true && this.wait_counter >= 25) {  // 25 periodic() iterations is roughly a half second. adjust to taste.
-      // too long without an update. stop waiting, reset counter, clear map (too old to be useful)
-      this.waiting_for_response = false;
-      this.wait_counter = 0;
-      SmartDashboard.putString("AprilTagFinderTimedout", String.format("hit counter %s", System.currentTimeMillis()));
-      // Our tag data is too old, clear it:
-      this.tag_data.id = -1;
-      return;
-    }
-
-    if (this.waiting_for_response == false) {
-        // this.camera.requestAprilTags("0");  // 0 gets all visible tags
-        output_buffer[0] = 1;
-        output_buffer[1] = 3; // Request april tags.
-        output_buffer[2] = (byte) this.tagID; // Request specific tag ID.
+      this.tagData.id = -1;
+    } else if (this.waiting_for_response == false) {
+       // We're not waiting for an answer, so send a new request.
+        outputBuffer[0] = (byte) (this.camID & 0xFF); // Camera ID.
+        outputBuffer[1] = 3; // Command: Find april tag = 3
+        outputBuffer[2] = (byte) (this.tagID & 0xFF); // Request specific tag ID.
 
         // Send request to camerea:
-        this.serialcomms.send(output_buffer);
-        this.send_counter +=1;
+        this.serialcomms.send(outputBuffer);
+        this.sendCounter +=1; // We sent a packet.
        
-
+        // We're now waiting for a response.
         this.waiting_for_response = true;
         this.wait_counter = 0;
-    }
-    else {
-      // We are waiting for a response:
-      var response = this.serialcomms.receive();
+    } else { 
+      // waiting_for_response = true...
 
-      if (response != null) {
-        parseResponse(response);
-        this.recv_counter += 1;
-        this.wait_counter = 0;
+       // Too long without an update. stop waiting, reset counter, clear tag data (too old to be useful)
+      if (this.wait_counter >= 25) {
         this.waiting_for_response = false;
+        this.wait_counter = 0;
+        SmartDashboard.putString("AprilTagFinderTimedout", String.format("hit counter %s", System.currentTimeMillis()));
+        // Our tag data is too old, clear it:
+        this.tagData.id = -1;
       } else {
-          // Else we don't yet have a full response.
-          this.wait_counter += 1; // We've waited a whole cycle...
+        // We are waiting for a response:
+        var response = this.serialcomms.receive();
+        if (response != null) {
+          parseResponse(response);
+          this.recvCounter += 1; // We received a packet.
+          this.wait_counter = 0; // We're not waiting anymore.
+          this.waiting_for_response = false;
+        } else {
+            // Else we don't yet have a full response.
+            this.wait_counter += 1; // Count the cycles we're waiting.
+        }
       }
     }
 
-    SmartDashboard.putNumber("AprilTag SendCounter",send_counter);
-    SmartDashboard.putNumber("AprilTag RecvCounter",recv_counter);
+    SmartDashboard.putNumber("AprilTag SendCounter", sendCounter);
+    SmartDashboard.putNumber("AprilTag RecvCounter", recvCounter);
 
-    SmartDashboard.putNumber("AprilTagID", this.tag_data.id);
-    SmartDashboard.putNumber("ApriltTag X", this.tag_data.cx);
-    SmartDashboard.putNumber("AprilTag Y", this.tag_data.cy);
-    SmartDashboard.putNumber("AprilTag Area", this.tag_data.area);
+    SmartDashboard.putNumber("AprilTag ID", this.tagData.id);
+    SmartDashboard.putNumber("AprilTag X", this.tagData.cx);
+    SmartDashboard.putNumber("AprilTag Y", this.tagData.cy);
+    SmartDashboard.putNumber("AprilTag Area", this.tagData.area);
   }
 
 }
